ccc492ab51e2f204e99886051ee7386a1055a672
diesmaster <c.van.maarseveen@umail.leidenuniv.nl>
2020-06-03 15:45:20 +0200

    test10

--- zcash_primitives/src/transaction/mod.rs
+++ zcash_primitives/src/transaction/mod.rs
@@ -186,7 +186,7 @@ impl Transaction {
             ));
         }
 
-        let vin; // = Vector::read(&mut reader, TxIn::read)?;
+        let vin = Vector::read(&mut reader, TxIn::read)?;
         let vout = Vector::read(&mut reader, TxOut::read)?;
 
         let lock_time = reader.read_u32::<LittleEndian>()?;
